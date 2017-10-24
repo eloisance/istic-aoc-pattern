0d02f130252aa0475d95cb9cf590b25a0aa585d8
Jnsll <june.sallou@laposte.net>
2017-10-24 14:00:35 +0200

    creation of GetValue

---
+++ src/main/java/com/istic/aoc/GetValue.java
@@ -0,0 +1,15 @@
+package com.istic.aoc;
+
+import java.util.concurrent.Callable;
+
+public class GetValue implements Callable<Integer>{
+    @Override
+    public Integer call() throws Exception {
+        try{
+
+        }catch(Exception e){
+
+        }
+        return null;
+    }
+}
