b72112efedd460b45c0894f5cfa458ecb67b153a
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2017-11-20 18:02:25 +0100

    Add labels channel

--- src/main/java/com/istic/aoc/WindowController.java
+++ src/main/java/com/istic/aoc/WindowController.java
@@ -1,4 +1,13 @@
 package com.istic.aoc;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class WindowController {
+
+    @FXML private Label c1;
+    @FXML private Label c2;
+    @FXML private Label c3;
+    @FXML private Label c4;
+
 }
