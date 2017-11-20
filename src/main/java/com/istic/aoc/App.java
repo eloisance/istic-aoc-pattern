7fd6ce694a66d3e76ecf7cd61fc65f512bd336cf
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2017-11-20 16:48:13 +0100

    Add all channels in main

--- src/main/java/com/istic/aoc/App.java
+++ src/main/java/com/istic/aoc/App.java
@@ -12,14 +12,26 @@ public class App
 
         Observer<AsyncGenerator> display1 = new Display();
         Observer<AsyncGenerator> display2 = new Display();
+        Observer<AsyncGenerator> display3 = new Display();
+        Observer<AsyncGenerator> display4 = new Display();
 
         Channel c1 = new Channel();
         Channel c2 = new Channel();
+        Channel c3 = new Channel();
+        Channel c4 = new Channel();
 
         g.attach(c1);
         c1.setObserver(display1);
 
-        g.generate();
+        g.attach(c2);
+        c2.setObserver(display2);
 
+        g.attach(c3);
+        c3.setObserver(display3);
+
+        g.attach(c4);
+        c4.setObserver(display4);
+
+        g.generate();
     }
 }
