14a7e5d8fa04ac890d84a01b5b9e31a18869b939
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2017-11-21 08:05:02 +0100

    Init basic methods

--- src/main/java/com/istic/aoc/WindowController.java
+++ src/main/java/com/istic/aoc/WindowController.java
@@ -1,5 +1,6 @@
 package com.istic.aoc;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -9,5 +10,21 @@ public class WindowController {
     @FXML private Label c2;
     @FXML private Label c3;
     @FXML private Label c4;
+
+    public void onStartClickButton(ActionEvent actionEvent) {
+        System.out.println("onStartClickButton");
+    }
+
+    public void onStopClickButton(ActionEvent actionEvent) {
+        System.out.println("onStopClickButton");
+    }
+
+    public void onAtomicChoice(ActionEvent actionEvent) {
+        System.out.println("onAtomicChoice");
+    }
+
+    public void onSequentialChoice(ActionEvent actionEvent) {
+        System.out.println("onSequentialChoice");
+    }
 
 }
