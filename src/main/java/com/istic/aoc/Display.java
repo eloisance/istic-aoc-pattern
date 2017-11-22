01bec67ecb8d4a0990014dca664aaaa1eaa99d33
Jnsll <june.sallou@laposte.net>
2017-11-22 14:42:21 +0100

    ajout de l'attribut value dans le display

--- src/main/java/com/istic/aoc/Display.java
+++ src/main/java/com/istic/aoc/Display.java
@@ -6,13 +6,24 @@ import java.util.concurrent.ExecutionException;
 
 public class Display implements Observer<AsyncGenerator> {
 
+    int value;
+
     public void update(AsyncGenerator asyncGenerator) {
         try {
             System.out.println("value: " + asyncGenerator.getValue().get());
+            value = asyncGenerator.getValue().get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 }
