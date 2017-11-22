fe3bddb94af1c622b60a22d5e39c9c360832ae83
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2017-11-22 14:33:35 +0100

    Move code into initialize WindowController

--- src/main/java/com/istic/aoc/App.java
+++ src/main/java/com/istic/aoc/App.java
@@ -1,44 +1,13 @@
 package com.istic.aoc;
 
-import com.istic.aoc.generator.GeneratorImpl;
-import com.istic.aoc.generator.Generator;
-import com.istic.aoc.observer.Observer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application
-{
-    public static void main(String[] args)
-    {
-        Generator g = new GeneratorImpl();
-
-        Observer<AsyncGenerator> display1 = new Display();
-        Observer<AsyncGenerator> display2 = new Display();
-        Observer<AsyncGenerator> display3 = new Display();
-        Observer<AsyncGenerator> display4 = new Display();
-
-        Channel c1 = new Channel();
-        Channel c2 = new Channel();
-        Channel c3 = new Channel();
-        Channel c4 = new Channel();
-
-        g.attach(c1);
-        c1.setObserver(display1);
-
-        g.attach(c2);
-        c2.setObserver(display2);
-
-        g.attach(c3);
-        c3.setObserver(display3);
-
-        g.attach(c4);
-        c4.setObserver(display4);
-
-        g.generate();
-
+public class App extends Application {
+    public static void main(String[] args) {
         launch(args);
     }
 
--- src/main/java/com/istic/aoc/WindowController.java
+++ src/main/java/com/istic/aoc/WindowController.java
@@ -1,18 +1,57 @@
 package com.istic.aoc;
 
+import com.istic.aoc.generator.Generator;
+import com.istic.aoc.generator.GeneratorImpl;
+import com.istic.aoc.observer.Observer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class WindowController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WindowController implements Initializable {
 
     @FXML private Label c1;
     @FXML private Label c2;
     @FXML private Label c3;
     @FXML private Label c4;
 
+    private Generator g;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("initialize");
+
+        g = new GeneratorImpl();
+
+        Observer<AsyncGenerator> display1 = new Display();
+        Observer<AsyncGenerator> display2 = new Display();
+        Observer<AsyncGenerator> display3 = new Display();
+        Observer<AsyncGenerator> display4 = new Display();
+
+        Channel c1 = new Channel();
+        Channel c2 = new Channel();
+        Channel c3 = new Channel();
+        Channel c4 = new Channel();
+
+        g.attach(c1);
+        c1.setObserver(display1);
+
+        g.attach(c2);
+        c2.setObserver(display2);
+
+        g.attach(c3);
+        c3.setObserver(display3);
+
+        g.attach(c4);
+        c4.setObserver(display4);
+    }
+
     public void onStartClickButton(ActionEvent actionEvent) {
         System.out.println("onStartClickButton");
+        g.generate();
     }
 
     public void onStopClickButton(ActionEvent actionEvent) {
