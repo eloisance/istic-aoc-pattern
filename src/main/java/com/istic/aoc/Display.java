0ad46a73e46fe54e4bdd588d97d6527a4a7b74e6
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2017-11-22 15:08:45 +0100

    Remove value in Display

--- src/main/java/com/istic/aoc/Display.java
+++ src/main/java/com/istic/aoc/Display.java
@@ -6,24 +6,14 @@ import java.util.concurrent.ExecutionException;
 
 public class Display implements Observer<AsyncGenerator> {
 
-    int value;
-
     public void update(AsyncGenerator asyncGenerator) {
         try {
             System.out.println("value: " + asyncGenerator.getValue().get());
-            value = asyncGenerator.getValue().get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
     }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
+    
 }
