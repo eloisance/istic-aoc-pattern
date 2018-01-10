19fb63d6fa34b9f6ab2c1e3b9855e66d624703cb
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2018-01-10 14:35:43 +0100

    Move setAlgo method

--- src/main/java/com/istic/aoc/generator/Generator.java
+++ src/main/java/com/istic/aoc/generator/Generator.java
@@ -1,10 +1,8 @@
 package com.istic.aoc.generator;
 
 import com.istic.aoc.observer.AsyncSubject;
-import com.istic.aoc.strategy.DiffusionAlgorithm;
 
 public interface Generator extends AsyncSubject<Generator> {
     Integer getValue();
     void generate();
-    void setAlgo(DiffusionAlgorithm diffusionAlgorithm);
 }
--- src/main/java/com/istic/aoc/observer/AsyncSubject.java
+++ src/main/java/com/istic/aoc/observer/AsyncSubject.java
@@ -1,5 +1,7 @@
 package com.istic.aoc.observer;
 
+import com.istic.aoc.strategy.DiffusionAlgorithm;
+
 import java.util.ArrayList;
 
 public interface AsyncSubject<T> {
@@ -7,4 +9,5 @@ public interface AsyncSubject<T> {
     void detach(AsyncObserver<T> observer);
     void notifyObservers();
     ArrayList<AsyncObserver> getObservers();
+    void setAlgo(DiffusionAlgorithm diffusionAlgorithm);
 }
